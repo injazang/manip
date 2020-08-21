c2df8e3107a71ad465d30c9137c55f44ffe72bd4
ijyu <ijyu@mmc.kaist.ac.kr>
2020-08-21 19:44:41 +0900

    dataparallel

--- models/mantra.py
+++ models/mantra.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn.functional as F
 import torch_dct as dct
 import numpy as np
+from module.modules import augment
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, padding_mode='replicate', bias=True):
     """3x3 convolution with padding"""
@@ -28,6 +29,7 @@ class Preprocess(nn.Module):
         self.bayar = conv5x5(3, 3, bias=False)
         self.srm = conv5x5(3, 3, bias=False)
         self.srm_kernel = self._build_SRM_kernel()
+
 
     def _get_srm_list( self ) :
         # srm kernel 1
@@ -56,7 +58,7 @@ class Preprocess(nn.Module):
             this_ch_kernel[idx, :,:] = srm
             kernel.append( this_ch_kernel )
         kernel = np.stack(kernel, axis=0)
-        srm_kernel = torch.from_numpy(kernel).cuda().float()
+        srm_kernel = torch.from_numpy(kernel).float()
         return srm_kernel
 
     def forward(self, x):
@@ -68,93 +70,15 @@ class Preprocess(nn.Module):
 
 
 
-class BlockType1(nn.Module):
-
-    def __init__(self, inplanes, planes, groups=1):
-        super(BlockType1, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, groups=groups)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        return out
-
-
-class BlockType2(nn.Module):
-
-    def __init__(self, inplanes, planes, groups=1):
-        super(BlockType2, self).__init__()
-        self.type1 = BlockType1(inplanes, planes, groups=groups)
-        self.conv1 = conv3x3(planes, planes, groups=groups)
-        self.bn1 = nn.BatchNorm2d(planes)
-
-    def forward(self, x):
-        identity = x
-
-        out = self.type1(x)
-        out = self.conv1(out)
-        out = self.bn1(out)
-
-        out += identity
-        return out
-
-
-class BlockType3(nn.Module):
-
-    def __init__(self, inplanes, planes, groups=1):
-        super(BlockType3, self).__init__()
-        self.conv0 = conv1x1(inplanes, planes, stride=2, groups=groups)
-        self.bn0 = nn.BatchNorm2d(planes)
-
-        self.type1 = BlockType1(inplanes, planes, groups=groups)
-        self.conv1 = conv3x3(planes, planes, groups=groups)
-        self.bn1 = nn.BatchNorm2d(planes)
-
-        self.avgpool = nn.AvgPool2d(3, 2, padding=1)
-
-    def forward(self, x):
-        identity = self.bn0(self.conv0(x))
-
-        out = self.type1(x)
-        out = self.conv1(out)
-        out = self.bn1(out)
-        out = self.avgpool(out)
-
-        out += identity
-        return out
-
-
-class BlockType4(nn.Module):
-
-    def __init__(self, inplanes, planes, groups=1):
-        super(BlockType4, self).__init__()
-        self.type1 = BlockType1(inplanes, planes, groups=groups)
-        self.conv1 = conv3x3(planes, planes, groups=groups)
-        self.bn1 = nn.BatchNorm2d(planes)
-
-    def forward(self, x):
-        out = self.type1(x)
-        out = self.conv1(out)
-        out = self.bn1(out)
-        #out = self.gvp(out)
-
-        return out
-
-
 class MantrNet(nn.Module):
     def __init__(self):
         super(MantrNet, self).__init__()
-
+        self.aug = augment()
         self.inplanes = 16
         self.name = 'srnet'
         self.pre = Preprocess()
-        self.conv_layers = self._make_layer([32,64,64,96,96,128,128,128,128])
-        self.last = conv3x3(in_planes=self.inplanes, out_planes=5)
-        self.sc = nn.Softmax()
+        self.conv_layers = self._make_layer([32,64,64, 128,128,256,256,256,256,256])
+        self.last = conv3x3(in_planes=self.inplanes, out_planes=20)
         self.conv_weights = []
         self.non_conv_weights = []
 
@@ -166,9 +90,9 @@ class MantrNet(nn.Module):
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_uniform_(m.weight)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0.2)
+                if m.weight.requires_grad==True:
+                    nn.init.kaiming_uniform_(m.weight)
+
             elif isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, mean=0.0, std=0.01)
 
@@ -176,8 +100,9 @@ class MantrNet(nn.Module):
         self.pre.srm.weight.requires_grad = False
         self.mask =np.zeros([3,3,5,5])
         self.mask[:,:,2,2]=1
-        self.mask = torch.from_numpy(self.mask).cuda()
+        self.mask = nn.Parameter(torch.from_numpy(self.mask))
         self.mask.requires_grad=False
+        self.constraint()
 
     def constraint(self):
         w = self.pre.bayar.weight.data
@@ -196,10 +121,10 @@ class MantrNet(nn.Module):
         layers.append(conv3x3(self.inplanes, num_filters[-1], groups=groups, bias=True))
         return nn.Sequential(*layers)
 
-    def forward(self, input):
+    def forward(self, input, prob):
+        input = self.aug(input, prob)
         x = self.pre(input)
         x = self.conv_layers(x)
         x = nn.functional.normalize(x, p=2)
         x = self.last(x)
-        x = self.sc(x)
         return x
